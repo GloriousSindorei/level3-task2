f0b279707755841fcabc606b0c3338b96960f15f
bkristel <bart.kristel@hotmail.com>
2020-09-16 14:07:53 +0200

    start task 2

---
+++ app/src/main/java/com/example/madlevel3task2/PortalAdapter.kt
@@ -0,0 +1,9 @@
+package com.example.madlevel3task2
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class PortalAdapter(private val portals: List<Portal>) : RecyclerView.Adapter<PortalAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View)
+}
